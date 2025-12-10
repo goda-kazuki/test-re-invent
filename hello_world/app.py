14fb0c3818d8e0091ef32f80d340f016d2d20295
goda-kazuki <kazuki.goda@serverworks.co.jp>
2025-12-10 14:36:19 +0900

    ビルド設定を更新し、依存関係を追加。コードの整形を行い、不要な出力を削除。

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,6 +1,7 @@
 import json
 import requests
 
+
 def lambda_handler(event, context):
     """Sample pure Lambda function
 
@@ -25,8 +26,7 @@ def lambda_handler(event, context):
 
     return {
         "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-            "location": ip.text.replace("\n", "")
-        }),
+        "body": json.dumps(
+            {"message": "hello world", "location": ip.text.replace("\n", "")}
+        ),
     }
